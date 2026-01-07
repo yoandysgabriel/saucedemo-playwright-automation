802dbc5dc4136b1fafe59189367d9b0ffcbaa12d
Sánchez Álvarez, Yoandys G <gabriel.sanchezalvarez@cgi.com>
2026-01-07 12:41:25 -0500

    remove comment sp

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -1,13 +1,11 @@
 class CheckoutPage {
   constructor(page) {
     this.page = page;
-    // Paso 1: Información
     this.firstNameInput = page.getByTestId('firstName');
     this.lastNameInput = page.getByTestId('lastName');
     this.postalCodeInput = page.getByTestId('postalCode');
     this.continueButton = page.getByTestId('continue');
 
-    // Paso 2: Resumen y Finalización
     this.finishButton = page.getByTestId('finish');
     this.completeHeader = page.getByTestId('complete-header');
   }
